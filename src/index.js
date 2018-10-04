fcb017721feef649289554e0678be60854221bca
Alex Wilmer <djazium@gmail.com>
2018-10-03 21:04:23 -0400

    🎶

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './index.css'
+// scene 1: super basic
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let shapes = ["sine", "square", "sawtooth", "triangle"]
+let ctx = new AudioContext()
+// let osc = ctx.createOscillator()
+// osc.connect(ctx.destination)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+let frequency = 1000
+let oscillators = {}
+// window.onmousemove = event =>
+//   osc && (osc.frequency.value = event.clientX)
+window.onmousemove = event => {
+  document.body.style.backgroundColor =
+    `hsl(${event.clientX / 3}, 70%, 70%)`
+  frequency = event.clientX
+  Object.values(oscillators).forEach(osc =>
+    osc.frequency.value = frequency
+  )
+}
+
+// osc.start()
+
+// nope
+// window.onkeydown = event =>
+//   osc.start()
+
+
+window.onkeydown = event => {
+  if (!oscillators[event.key]) {
+    let osc = ctx.createOscillator()
+    osc.frequency.value = frequency
+    osc.type = shapes[event.keyCode % shapes.length]
+    osc.connect(ctx.destination)
+    osc.start()
+
+    oscillators[event.key] = osc
+  }
+}
+
+window.onkeyup = event => {
+  oscillators[event.key].stop()
+  delete oscillators[event.key]
+}
