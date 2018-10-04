43bd5da287e50f864ba24faa9b022692be184054
Alex Wilmer <djazium@gmail.com>
2018-10-03 22:52:21 -0400

    audiokeys !!!

--- src/index.js
+++ src/index.js
@@ -1,10 +1,52 @@
 import './index.css'
+import AudioKeys from 'audiokeys'
 // scene 1: super basic
 
+let keyboard = new AudioKeys()
 let shapes = ["sine", "square", "sawtooth", "triangle"]
 let ctx = new AudioContext()
 // let osc = ctx.createOscillator()
 // osc.connect(ctx.destination)
+
+keyboard.down(note => {
+  if (!sources[note.keyCode]) {
+    document.body.style.backgroundColor =
+      `hsl(${note.keyCode * 10}, 70%, 70%)`
+
+    let osc = ctx.createOscillator()
+    let gain = ctx.createGain()
+
+    gain.gain.value = 0
+    gain.gain.linearRampToValueAtTime(
+      1,
+      ctx.currentTime + 0.1
+    )
+
+    osc.frequency.value = note.frequency
+    osc.type = shapes[note.keyCode % shapes.length]
+
+    osc.connect(gain)
+    gain.connect(ctx.destination)
+
+    osc.start()
+
+    sources[note.keyCode] = {
+      osc,
+      gain
+    }
+  }
+  // do things with the note object
+});
+
+keyboard.up(note => {
+  // do things with the note object
+    sources[note.keyCode].gain.gain.linearRampToValueAtTime(
+      0,
+      ctx.currentTime + 0.4
+    )
+    sources[note.keyCode].osc.stop(ctx.currentTime + 0.5)
+    delete sources[note.keyCode]
+});
 
 let frequency = 1000
 let sources = {}
@@ -27,36 +69,38 @@ window.onmousemove = event => {
 
 
 window.onkeydown = event => {
-  if (!sources[event.key]) {
-    let osc = ctx.createOscillator()
-    let gain = ctx.createGain()
+  // console.log(event.keyCode)
 
-    gain.gain.value = 0
-    gain.gain.linearRampToValueAtTime(
-      1,
-      ctx.currentTime + 0.1
-    )
-
-    osc.frequency.value = frequency
-    osc.type = shapes[event.keyCode % shapes.length]
-
-    osc.connect(gain)
-    gain.connect(ctx.destination)
-
-    osc.start()
-
-    sources[event.key] = {
-      osc,
-      gain
-    }
-  }
+  // if (!sources[event.key]) {
+  //   let osc = ctx.createOscillator()
+  //   let gain = ctx.createGain()
+  //
+  //   gain.gain.value = 0
+  //   gain.gain.linearRampToValueAtTime(
+  //     1,
+  //     ctx.currentTime + 0.1
+  //   )
+  //
+  //   osc.frequency.value = frequency
+  //   osc.type = shapes[event.keyCode % shapes.length]
+  //
+  //   osc.connect(gain)
+  //   gain.connect(ctx.destination)
+  //
+  //   osc.start()
+  //
+  //   sources[event.key] = {
+  //     osc,
+  //     gain
+  //   }
+  // }
 }
 
-window.onkeyup = event => {
-  sources[event.key].gain.gain.linearRampToValueAtTime(
-    0,
-    ctx.currentTime + 0.4
-  )
-  sources[event.key].osc.stop(ctx.currentTime + 0.5)
-  delete sources[event.key]
-}
+// window.onkeyup = event => {
+//   sources[event.key].gain.gain.linearRampToValueAtTime(
+//     0,
+//     ctx.currentTime + 0.4
+//   )
+//   sources[event.key].osc.stop(ctx.currentTime + 0.5)
+//   delete sources[event.key]
+// }
